9dad60114b7fef8482d0e6441f8905016b7e04c2
caliskanadem <ad3m.calikan@gmail.com>
2023-06-04 04:01:33 +0100

    form

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,7 +1,7 @@
 const ProductForm = ({ formData, handleChange, handleSubmit, text }) => {
   return (
     <article id="add-product" className="mb-4 mt-4">
-      <h1 className="text-center">{text} Product</h1>
+      <h1 className="text-center text-secondary">{text} Product</h1>
       <form className="p-2" onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="add-name" className="form-label">
