039ef57a3c587f2c27cc8655edf81575978ca855
caliskanadem <ad3m.calikan@gmail.com>
2023-06-04 03:12:52 +0100

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      <h1 className='text-center m-5'>About</h1>
+      <h1 className="text-center m-5">
+        <div>
+          <img src="face-head.png" style={{ width: "300px" }} alt="adam" />
+          <div>
+            <h2>Hi, I'm Adem</h2>
+            <h3>I’m learning Frond-End Development Languages.</h3>
+            <h4>I've already known JS, CSS, ReactNative, NodeJS...</h4>
+            <h4>💬 You can ask me anything you want to know</h4>
+          </div>
+        </div>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
